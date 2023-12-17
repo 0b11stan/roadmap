48cfe4026b7819bd1e048abf12a3b3cb41126e22
Tristan Pinaudeau <tristan@tic.sh>
2023-12-18 00:07:52 +0100

    update

--- plot_rank.py
+++ plot_rank.py
@@ -1,16 +1,20 @@
 from datetime import datetime as dt
 import matplotlib.pyplot as plt
+import os
 
 
-def read_data():
-    with open('rank.csv') as f:
+def read_data(username):
+    with open(f'ranks/{username}.csv') as f:
         logs = [log.strip() for log in f.readlines()][1:]
     timestamps = [dt.fromtimestamp(int(log.split(',')[1])) for log in logs]
     ranks = [int(log.split(',')[2]) for log in logs]
     return timestamps, ranks
 
 
-x, y = read_data()
 fig, ax = plt.subplots()
-ax.plot(x, y)
+
+for username in [f.split('.')[0] for f in os.listdir('ranks')]:
+    x, y = read_data(username)
+    ax.plot(x, y)
+
 plt.show()
--- watch_rank.sh
+++ watch_rank.sh
@@ -1,12 +1,20 @@
-OLDRANK=$(tail -n 1 rank.csv | cut -d ',' -f 3)
+USERNAMES="0b11stan Ad4m5 AlbertLingot P0wned"
 
-while true; do
+fetchrank() {
+  USERNAME=$1
+  USERFILE=ranks/$USERNAME.csv
+  OLDRANK=$(tail -n 1 $USERFILE | cut -d ',' -f 3)
   TIMESTAMP=$(date +%s)
   DATETIME=$(date -d @$TIMESTAMP "+%Y-%m-%d %H:%M:%S")
-  RANK=$(curl -s https://tryhackme.com/api/user/rank/0b11stan | jq .userRank)
+  RANK=$(curl -s https://tryhackme.com/api/user/rank/$USERNAME | jq .userRank)
   if [[ "$RANK" != "$OLDRANK" ]]; then
-    echo "$DATETIME,$TIMESTAMP,$RANK" | tee -a rank.csv
-    OLDRANK=$RANK
+    echo "$DATETIME,$TIMESTAMP,$RANK" | tee -a $USERFILE
   fi
+}
+
+while true; do
+  for USERNAME in $USERNAMES; do
+    fetchrank $USERNAME
+  done
   sleep 60
 done
